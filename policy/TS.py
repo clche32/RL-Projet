e22d47ec1307bd9b39391a7bcee3b54ba2138a10
LudovicR <ludo1093@gmail.com>
2022-11-23 11:18:42 -0500

    ajout de f_dsw

--- policy/TS.py
+++ policy/TS.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def discounted_ts(bandit, T, alpha, beta, gamma, seed=None):
+def discounted(bandit, T, alpha, beta, gamma, seed=None):
     '''Play the given non-stationary bandit over T rounds using the
     discounted TS strategy for Bernoulli bandits with given priors
     alpha and beta, discount factor gamma, and (optional) random seed.'''
@@ -30,7 +30,7 @@ def discounted_ts(bandit, T, alpha, beta, gamma, seed=None):
         S[k_t] += r_t
         F[k_t] += 1-r_t
 
-def sliding_window_ts(bandit, T, alpha, beta, tau, seed=None):
+def sliding_window(bandit, T, alpha, beta, tau, seed=None):
     '''Play the given non-stationary bandit over T rounds using the
     sliding window TS strategy for Bernoulli bandits with given priors
     alpha and beta, window size tau, and (optional) random seed.'''
@@ -61,10 +61,63 @@ def sliding_window_ts(bandit, T, alpha, beta, tau, seed=None):
         reward_obtained.append(r_t)
 
         # update posterior parameters
-        if t > tau:
-            k = action_played[t-tau-1]
-            r = reward_obtained[t-tau-1]
+        if t >= tau:
+            k = action_played[t-tau]
+            r = reward_obtained[t-tau]
             S[k] -= r
             F[k] -= 1-r
         S[k_t] += r_t
         F[k_t] += 1-r_t
+
+def f_dsw(bandit, T, alpha, beta, gamma, tau, f, seed=None):
+    '''Play the given non-stationary bandit over T rounds using the
+    f-dsw TS strategy for Bernoulli bandits with given priors alpha
+    and beta, discount factor gamma, window size tau, function f,
+    and (optional) random seed.'''
+    K = bandit.get_K()
+
+    # to compute the posterior beta distribution for each action
+    # i.e : Beta(alpha + S_k, beta + F_k)
+    S_d = np.zeros(K)
+    F_d = np.zeros(K)
+    S_sw = np.zeros(K)
+    F_sw = np.zeros(K)
+
+    # NEW : to track the action played and reward obtained at time t
+    # in order to subtract it from S and F when t is no longer in the window
+    action_played = []
+    reward_obtained = []
+
+    random = np.random.RandomState(seed)
+
+    for t in range(T):
+        # draw random samples
+        samples_d = np.zeros(K)
+        samples_sw = np.zeros(K)
+        samples_max = np.zeros(K)
+        for k in range(K):
+            samples_d[k] = random.beta(alpha + S_d[k], beta + F_d[k])
+            samples_sw[k] = random.beta(alpha + S_sw[k], beta + F_sw[k])
+            samples_max[k] = f(samples_d[k], samples_sw[k])
+
+        # play the action with highest sample
+        k_t = np.argmax(samples_max)
+        r_t = bandit.play(k_t, t)
+        action_played.append(k_t)
+        reward_obtained.append(r_t)
+
+        # update posterior parameters (discounted)
+        for k in range(K):
+            S_d[k] = gamma*S_d[k]
+            F_d[k] = gamma*F_d[k]
+        S_d[k_t] += r_t
+        F_d[k_t] += 1-r_t
+
+        # update posterior parameters (sliding window)
+        if t >= tau:
+            k = action_played[t-tau]
+            r = reward_obtained[t-tau]
+            S_sw[k] -= r
+            F_sw[k] -= 1-r
+        S_sw[k_t] += r_t
+        F_sw[k_t] += 1-r_t
